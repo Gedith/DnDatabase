6287fa6e1c61524455709e43d5c912f15010e795
Tomáš Sobotka <tomas.sobotka@uhk.cz>
2024-03-28 17:20:00 +0100

    Funkční přihlašování s pomocí express-sessions

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
+const session = require('express-session')
 const mysql = require('mysql')
 const bcrypt = require('bcrypt')
 const saltRounds = 10
-
 
 
 sql = mysql.createConnection({
@@ -23,6 +23,11 @@ app.listen(3000)
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended:true}))
 app.use(express.static('public'))
+app.use(session({
+    secret: 'secret_key',
+    resave: false,
+    saveUninitialized: false
+}))
 
 app.get('/', (req, res) => {
     res.render('login')
@@ -36,6 +41,7 @@ app.post('/register', (req,res) => {
     let name = Object.values(req.body)[0]
     let pass = Object.values(req.body)[1]
     bcrypt.hash(pass, saltRounds, (err, hash) => {
+        if(err) throw err
         sql.query("INSERT INTO `uzivatele`(`Heslo`, `Jmeno`) VALUES ('"+hash+"','"+name+"')", (err,res) => {
             if(err) throw err
             console.log(res)
@@ -44,18 +50,28 @@ app.post('/register', (req,res) => {
     res.render('login')
 })
 
+app.get('/home', (req, res) => {
+    res.render('home')
+})
+
 app.post('/', (req,res) => {
     let name = Object.values(req.body)[0]
     let pass = Object.values(req.body)[1]
-    sql.query("SELECT Jmeno, Heslo FROM `uzivatele` WHERE Jmeno = '"+name+"'", (err, res) => {
+    sql.query("SELECT Jmeno, Heslo FROM `uzivatele` WHERE Jmeno = '"+name+"'", (err, sqlResult) => {
         if(err) throw err
-        if(res.length > 0){
-            bcrypt.compare(pass, res[0].Heslo, (err, res) => {
-                console.log(res)
+        if(sqlResult.length > 0){
+            bcrypt.compare(pass, sqlResult[0].Heslo, (err, compare) => {
+                if(compare == true){
+                    req.session.userName = name
+                    res.render('home', { userName: req.session.userName })
+                } else {
+                    console.log('Wrong password')
+                    res.render('login')
+                }
             })
         } else {
             console.log('Noone was found in the database')
+            res.render('login')
         }
     })
-    res.render('login')
 })
