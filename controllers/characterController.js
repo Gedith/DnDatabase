fee4a9d70849daee832a5c977dd29f9938174fe6
Tomáš Sobotka <tomas.sobotka@uhk.cz>
2024-03-30 01:52:58 +0100

    Funkční model pro charaktery + základ stránky na detail charakteru

--- controllers/characterController.js
+++ controllers/characterController.js
@@ -1,3 +1,5 @@
+const characterModule = require('../module/characterModule')
+
 const indexCreateCharacter = (req, res) => {
     res.render('createCharacter')
 }
@@ -9,13 +11,14 @@ const createCharacter = (req, res) => {
     res.redirect('/home')
 }
 
-const characterDetails = (req, res) => {
+const characterDetails = async (req, res) => {
     const id = req.params.id
-    sql.query("SELECT * FROM hracskepostavy WHERE hracskepostavy.HracskepostavyID = "+id, (err, sqlResult) => {
-        if(err) throw err    
-        res.render('characterDetails', { character: sqlResult[0] })
+    await characterModule.getCharacterData(id)
+    .then((message) => {
+        res.render('characterDetails', { character: message })
+    }).catch((message) => {
+        console.log('catch message:'+message)
     })
-
 }
 
 module.exports = {
---
+++ module/characterModule.js
@@ -0,0 +1,19 @@
+getCharacterData =  (id) => {
+    return new Promise( (resolve, reject) => {
+        sql.query("SELECT * FROM hracskepostavy WHERE hracskepostavy.HracskepostavyID = "+id, (err, sqlResult) => {
+            if(err) throw err 
+            const character = {
+                name: sqlResult[0].Jmeno,
+                class: sqlResult[0].Povolani,
+                race: sqlResult[0].Rasa,
+                level: sqlResult[0].Uroven,
+            }
+            resolve(character)
+        })
+    })    
+ 
+}
+
+module.exports = {
+    getCharacterData
+}
